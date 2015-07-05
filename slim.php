0b4a4e28ddd2e09f4be5cc16df50b5340f7d2953
Jessica Straw <jcstraw3@gmail.com>
2015-07-04 22:25:01 -0400

    route passing through a function

--- slim.php
+++ slim.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/', function(){
-	echo "Hello World";
+$app->get('/hello/:name', function($name){
+	echo "Hello $name";
 });
 
 $app->run();
